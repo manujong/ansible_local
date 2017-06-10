f21ecd644dcadad8ea2657e3dcf922485a44a2f5
Emmanuel Dubois <emmanuel.dubois@cnes.fr>
2017-06-10 19:27:11 +0200

    update and simplify playbooks with vault protection for password

---
+++ default_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Package perso script to launch ansible site.yml
+#   
+#   
+#   Copyright © 2016 Emmanuel Dubois
+#    
+#   
+#   @file     default_install.sh
+#   @brief    This script is used to install personal default packages and default configuration as backup
+#   @author   Emmanuel Dubois <dubois.manu@laposte.net>
+
+sudo ansible-playbook -i hosts  -e secret --vault-password-file ~/.vault_pass.txt ~/sys/dev/local/default_install/site.yml
+
